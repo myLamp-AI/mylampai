358d7559e2f8d1c10b1e2cace669ca2467daab5d
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-05-10 20:33:11 +0530

    update

--- components/sideBar02.tsx
+++ components/sideBar02.tsx
@@ -4,14 +4,20 @@ import Image from "next/image";
 import { useState } from "react";
 
 
-export default function SideBar02(){
-    const [open, setOpen]= useState<boolean>(false);
-    console.log(open);
-    const dropHandle=(state:boolean)=>{
-        setOpen(!state);
-    
+export default function SideBar(){
+     const [isOpen1, setIsOpen1] = useState(false);
+     const toggleDropdown1 = () => {
+     setIsOpen1(!isOpen1);
+     };
+     const [isOpen2, setIsOpen2] = useState(false);
+     const toggleDropdown2 = () => {
+     setIsOpen2(!isOpen2);
+     };
+     const [isOpen3, setIsOpen3] = useState(false);
+     const toggleDropdown3 = () => {
+     setIsOpen3(!isOpen3);
+  };
 
-    }
     return (
     <>
      <div className=" h-screen flex ...">
@@ -19,7 +25,7 @@ export default function SideBar02(){
      <div className="flex absolute inset-x-0 h-24 mt-2 mr-3 ml-2 mb-0 rounded-lg bottom-0 bg-white ...">
         <div className="bg-[#8C52FF] w-1/3 rounded-lg ... ">
         <div className="rounded-full h-16 w-16 flex items-center justify-center bg-white mt-4 ml-5"><Image
-          src="/Vector-3.svg"
+          src="/Vector-2.svg"
           alt="Verceldcjncn Logo"
           className="dark:invert"
            width={45}
@@ -28,9 +34,27 @@ export default function SideBar02(){
             </div>
             
         </div>
-        <div className="w-2/5 bg-white rounded-lg ..."></div>
+        <div className="w-2/5 bg-white rounded-lg ..."> <div className="h-fit absolute  right-50 top-6 ">
+      <div>
+        <span className="text-left  text-black font-bold text-decoration-line: underline font-size: 0.75rem ...">Capstone Project</span>
+        </div>
+        <div>
+        <span className=" text-left text-black font-bold">Tech 101: Starting in Tech</span>
+        </div>
+        </div></div>
      </div>  
      <div className="h-24 mt-2 mr-3 ml-2 mb-0 h-30 bg-[#8C52FF] rounded-lg ...">
+     <div className="h-fit absolute  right-14 top-6 ">
+      <div>
+        <span className="text-left  text-white font-bold text-decoration-line: underline font-size: 0.75rem ...">module 03</span>
+        </div>
+        <div>
+        <span className=" text-left text-white font-bold">Tech 101: Starting in Tech</span>
+        </div>
+        <div>
+        <span className=" text-left text-white text-[10px]"> 7 lesson   7 lesson    7 lesson    </span>
+        </div>
+        </div>
      <div className="rounded-full h-14 w-14 flex items-center justify-center bg-white mt-4 ml-2"><Image
           src="/ai.png"
           alt="Verceldcjncn Logo"
@@ -39,7 +63,8 @@ export default function SideBar02(){
            height={35}
             />
             </div>
-            <div  onClick={e=>dropHandle(true)} className="rounded-full h-3 w-3  flex items-center justify-center bg-white mr-3 -mt-7 ml-80"><Image
+           
+            <div className="rounded-full h-3 w-3  flex items-center justify-center bg-white mr-3 -mt-7 ml-80"><Image
           src="/arrow.png"
           alt="Verceldcjncn Logo"
           className="dark:invert"
@@ -50,8 +75,16 @@ export default function SideBar02(){
      
      </div>
      <div className="absolute inset-x-0 top-28 h-24 mt-2 mr-3 ml-2 mb-0 h-30 bg-[#f1f0f4] rounded-lg ...">
+       <div className="h-fit absolute right-32 top-6 ">
+          <div>
+        <span className="text-left font-bold font-size: 0.75rem ...">Grade 47%</span>
+        </div>
+        <div>
+        <span className=" text-left font-bold">Credit 4/12</span>
+        </div>
+        </div>
      <div className="rounded-full h-14 w-14 flex items-center justify-center  mt-6 ml-2"><Image
-          src="/robot-2.svg"
+          src="/robot.svg"
           alt="Verceldcjncn Logo"
           className="dark:invert"
            width={55}
@@ -68,13 +101,69 @@ export default function SideBar02(){
             </div>
      </div>
      <div className="flex justify-between absolute inset-x-0 top-56 h-12 mt-2 mr-3 ml-2 mb-0 h-30 rounded-lg ...">
-        <div className="bg-white text-sm  mt-2 mr-4 h-8 inset-y-0 text-centerw-16 ...">insights</div>
+        <div className="bg-white text-[15px] mt-2 mr-4 h-8 inset-y-0 text-centerw-16 ...">insights</div>
         <div className="bg-white mt-2 mr h-8 inset-y-0 left-0 w-16 text-sm  ...">Roadmap</div>
         <div className="bg-white mt-2 mr-10 h-8 inset-y-0 left-0 w-16 text-sm  ...">logo</div>
      </div>
       
       <div className="h-96 mt-2 mr-0 ml-0 mb-0 h-30 rounded-lg ... ">
-      <div className="h-28 mt-2 mr-3 ml-2  mb-0 h-30 relative bg-[#8C52FF] rounded-lg ...">
+      <div onClick={toggleDropdown1} className="h-28 mt-2 mr-3 ml-2  mb-0 h-30 relative bg-[#8C52FF] rounded-lg ...">
+      {isOpen1 && (
+     <div className="float absolute bg-white h-fit border mt-16 border-gray-300 w-full  ml-30 rounded-md shadow-lg z-10 -right-0">
+         
+     <div className="bg-[#E8E2F4] w-80 ml-2 relative h-10 mt-3 border-solid border-2 border-[#8C52FF] flex justify-center items-center rounded-full ..."> 
+          
+          <div className="h-fit absolute left-3"><Image
+          src="/play.svg"
+          alt="Verceldcjncn Logo"
+          className="dark:invert"
+           width={25}
+           height={25}
+            />
+            </div>
+            Language of ML: Python</div>
+     <div className="bg-[#E8E2F4] relative w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] flex justify-center items-center rounded-full ...">
+     <div className="h-fit absolute left-3"><Image
+          src="/play.svg"
+          alt="Verceldcjncn Logo"
+          className="dark:invert"
+           width={25}
+           height={25}
+            />
+            </div>
+      Language of ML: Phython</div>
+     <div className="bg-[#E8E2F4] relative w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] flex justify-center items-center rounded-full ...">
+     <div className="h-fit absolute left-3"><Image
+          src="/play.svg"
+          alt="Verceldcjncn Logo"
+          className="dark:invert"
+           width={25}
+           height={25}
+            />
+            </div>Language of ML: Phython</div>
+     <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 relative border-solid border-2 border-[#8C52FF] flex justify-center items-center rounded-full ...">
+     <div className="h-fit absolute left-3"><Image
+          src="/play.svg"
+          alt="Verceldcjncn Logo"
+          className="dark:invert"
+           width={25}
+           height={25}
+            />
+            </div>
+            Language of ML: Phython</div>
+     <div className="bg-[#E8E2F4] w-80 ml-2 mb-3 h-10 mt-3 relative border-solid border-2 border-[#8C52FF] flex justify-center items-center rounded-full ...">
+     <div className="h-fit absolute left-3"><Image
+          src="/play.svg"
+          alt="Verceldcjncn Logo"
+          className="dark:invert"
+           width={25}
+           height={25}
+            />
+            </div>
+            Language of ML: Phython</div>
+
+    </div>
+      )}
       <div className="absolute bottom-3 right-3 h-3 w-3 flex  bg-white rounded-full ...">
       <Image
           src="/arrow.png"
@@ -86,17 +175,36 @@ export default function SideBar02(){
       </div>
       
       <div className="h-20 relative mb-0 h-30 bg-[#e9e8eb] rounded-lg ...">
+      <div className="h-fit absolute right-14 top-3 ">
+          <div>
+        <span className="text-left   text-decoration-line: underline font-size: 0.75rem ...">chapter 2</span>
+        </div>
+        <div>
+        <span className=" text-left">Development Frameworks</span>
+        </div>
+        </div>
       <div className="rounded-full h-12 w-12 flex items-center justify-center bg-[#E8E2F4] absolute bottom-4 left-2.5"><Image
-          src="/ml icon.svg"
-          alt="Verceldcjncn Logo"
-          className="dark:invert"
+           src="/ml icon.svg"
+           alt="Verceldcjncn Logo"
+           className="dark:invert"
            width={25}
            height={25}
             />
             </div>
       </div>
       </div>
-      <div className=" relative h-28 mt-2 mr-3 ml-2 mb-0 h-30 bg-[#8C52FF] rounded-lg ...">
+      <div onClick={toggleDropdown2} className=" relative h-28 mt-2 mr-3 ml-2 mb-0 h-30 bg-[#8C52FF] rounded-lg ...">
+      {isOpen2 && (
+       <div className="float absolute bg-white h-fir border mt-16 border-gray-300 w-full  ml-30 rounded-md shadow-lg z-10 -right-0">
+         
+       <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+       <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+       <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+       <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+       <div className="bg-[#E8E2F4] w-80 ml-2 mb-3 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+      
+      </div>
+      )}
       <div className="absolute onclick() bottom-3 right-3 h-3 w-3 flex  bg-white rounded-full ...">
       <Image
           src="/arrow.png"
@@ -107,6 +215,14 @@ export default function SideBar02(){
             />
       </div>
       <div className="h-20  mb-0 h-30 bg-[#e9e8eb] relative rounded-lg ...">
+        <div className="h-fit absolute right-14 top-3 ">
+          <div>
+        <span className="text-left   text-decoration-line: underline font-size: 0.75rem ...">chapter 2</span>
+        </div>
+        <div>
+        <span className=" text-left">Development Frameworks</span>
+        </div>
+        </div>
       <div className="rounded-full h-12 w-12 flex items-center justify-center bg-[#E8E2F4] absolute bottom-4 left-2.5"><Image
           src="/developement.svg"
           alt="Verceldcjncn Logo"
@@ -117,7 +233,19 @@ export default function SideBar02(){
             </div>
       </div>
       </div>
-      <div className=" relative h-28 mt-2 mr-3 ml-2 mb-0 h-30 bg-[#8C52FF] rounded-lg ...">
+      <div onClick={toggleDropdown3}  className=" relative h-28 mt-2 mr-3 ml-2 mb-0 h-30 bg-[#8C52FF] rounded-lg ...">
+      {isOpen3 && (
+        <div className="float absolute bg-white h-fir border mt-16 border-gray-300 w-full  ml-30 rounded-md shadow-lg z-10 -right-0">
+         
+         <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+         <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+         <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+         <div className="bg-[#E8E2F4] w-80 ml-2 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+         <div className="bg-[#E8E2F4] w-80 ml-2 mb-3 h-10 mt-3 border-solid border-2 border-[#8C52FF] rounded-full ..."></div>
+        
+        </div>
+        
+      )}
       <div className="absolute bottom-3 right-3 h-3 w-3 flex  bg-white rounded-full ...">
       <Image
           src="/arrow.png"
@@ -128,6 +256,14 @@ export default function SideBar02(){
             />
       </div>
       <div className="h-20 relative mb-0 h-30 bg-[#e9e8eb] rounded-lg ...">
+      <div className="h-fit absolute right-14 top-3 ">
+          <div>
+        <span className="text-left   text-decoration-line: underline font-size: 0.75rem ...">chapter 2</span>
+        </div>
+        <div>
+        <span className=" text-left">Development Frameworks</span>
+        </div>
+        </div>
       <div className="rounded-full h-12 w-12 flex items-center justify-center bg-[#E8E2F4] absolute bottom-4 left-2.5"><Image
           src="/cybersceurity.svg"
           alt="Verceldcjncn Logo"
@@ -140,8 +276,7 @@ export default function SideBar02(){
       </div>
       </div>
     
-      
-  
+    
     </div>
     <div className=" relative w-3/4 ...">
 
