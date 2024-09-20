cf309b822092c5771b7b489e1ec76912d2a972a6
Rust <ashutoshkrsingh.iitkgp@gmail.com>
2024-09-20 22:03:03 +0530

    minor updates

--- app/(home)/wizecamp/page.tsx
+++ app/(home)/wizecamp/page.tsx
@@ -1,37 +1,19 @@
-"use client";
 import Image from "next/image";
-import Link from "next/link";
-import React, { useEffect, useRef, useState } from 'react';
 
 export default function WizeCamp() {
 
-  const [isContactOpen, setIsContactOpen] = useState(true);
-
-  const toggleContact = () => {
-    setIsContactOpen(!isContactOpen);
-  };
-
-  useEffect(() => {
-    if (!isContactOpen) {
-      document.body.classList.add("overflow-hidden");
-    } else {
-      document.body.classList.remove("overflow-hidden");
-    }
-  }, [!isContactOpen]);
-
-
   return (
     <>
-      <div className={`fixed top-[100vh] min-h-[calc(100vh-4rem)] w-full left-0 right-0 flex justify-center duration-500 z-10 transition ${isContactOpen ? "translate-y-[-300vh]" : "-translate-y-full"}`}>
+      <div className={`fixed top-[100vh] min-h-[calc(100vh-4rem)] w-full left-0 right-0 flex justify-center duration-500 z-10 transition `}>
         <div className="bg-white mt-10 w-[85vw] h-[80vh] flex flex-col items-center justify-center shadow-lg rounded-lg">
           <h1> I am a Reg. Form</h1>
-          <button onClick={toggleContact} className="relative text-white font-bold top-[30vh] p-4 border-x-primary-foreground border-4 rounded-xl bg-primary">
+          <button className="relative text-white font-bold top-[30vh] p-4 border-x-primary-foreground border-4 rounded-xl bg-primary">
             CLOSE FORM
           </button>
         </div>
       </div>
 
-      <div className={` transition ${!isContactOpen ? "blur-sm" : "blur-none"}`}>
+      <div className={` transition `}>
         <div className={`bg-primary min-h-[calc(100vh-4rem)] w-full flex justify-evenly items-center text-white p-4 `}>
           <div className="element xl:max-w-[1220px] max-w-[650px] sm:m-auto bg-transperent flex flex-col xl:flex-row justify-around items-center rounded-2xl p-4 sm:p-8 gap-36">
             <div className="bg-[#8C52FF] w-full h-full max-w-[600px] min-h-[500px] flex flex-col items-center justify-evenly rounded-2xl p-4">
@@ -155,7 +137,7 @@ export default function WizeCamp() {
               <div className='w-1/2 bg-white flex justify-center items-center'>img</div>
               <div className="relative flex flex-col justify-evenly w-1/2">
                 <p className="w-full relative max-h-1/2 2xl:text-sm  md:text-[0.9vw] ">Just relax and take the interview. Have your skills evaluated. All the best!</p>
-                <button onClick={toggleContact} className="bg-primary w-full relative 2xl:top-[2vh] rounded-lg px-4 py-2 font-semibold 2xl:text-lg md:text-[1vw] text-white">
+                <button className="bg-primary w-full relative 2xl:top-[2vh] rounded-lg px-4 py-2 font-semibold 2xl:text-lg md:text-[1vw] text-white">
                   Register
                 </button>
               </div>
